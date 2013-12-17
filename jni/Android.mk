7b5d235d0c2eca2d3db2920a8730dbd731ab073f
Jason Ekstrand <jason@jlekstrand.net>
2013-12-16 20:53:38 -0600

    Remove the demo native activity

--- jni/Android.mk
+++ jni/Android.mk
@@ -19,16 +19,6 @@ wheatley: wlb
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := demo-activity
-LOCAL_CFLAGS :=
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM
-LOCAL_SRC_FILES := demo_activity.c
-LOCAL_STATIC_LIBRARIES := android_native_app_glue
-
-include $(BUILD_SHARED_LIBRARY)
-
 include $(LOCAL_PATH)/test-clients/Android.mk
 
 $(call import-module, android/native_app_glue)
