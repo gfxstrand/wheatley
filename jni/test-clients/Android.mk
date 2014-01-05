b82eb8193717ba8bd7d4fa4670a0d92ef2e46108
Jason Ekstrand <jason@jlekstrand.net>
2014-01-04 21:10:45 -0600

    Add simple-touch to the test clients

--- jni/test-clients/Android.mk
+++ jni/test-clients/Android.mk
@@ -18,3 +18,17 @@ LOCAL_STATIC_LIBRARIES	:= libwayland-client
 simple-shm: wayland-client $(LOCAL_PATH)/fullscreen-shell-client-protocol.h
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE		:= simple-touch
+LOCAL_SRC_FILES		:=		\
+	fullscreen-shell-protocol.c	\
+	os-compatibility.c		\
+	simple-touch.c
+LOCAL_LDLIBS := -llog
+LOCAL_STATIC_LIBRARIES	:= libwayland-client
+
+simple-touch: wayland-client $(LOCAL_PATH)/fullscreen-shell-client-protocol.h
+
+include $(BUILD_EXECUTABLE)
---
+++ jni/test-clients/simple-touch.c
@@ -0,0 +1,359 @@
+/*
+ * Copyright © 2011 Benjamin Franzke
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+#include <wayland-client.h>
+#include "os-compatibility.h"
+#include "fullscreen-shell-client-protocol.h"
+
+#include <android/log.h>
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "simple-touch", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "simple-touch", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "simple-touch", __VA_ARGS__))
+
+#define ARRAY_LENGTH(a) (sizeof (a) / sizeof (a)[0])
+
+struct touch {
+	struct wl_display *display;
+	struct wl_registry *registry;
+	struct wl_compositor *compositor;
+	struct wl_shell *shell;
+	struct wl_fullscreen_shell *fullscreen_shell;
+	struct wl_shm *shm;
+	struct wl_seat *seat;
+	struct wl_touch *wl_touch;
+	struct wl_pointer *pointer;
+	struct wl_keyboard *keyboard;
+	struct wl_surface *surface;
+	struct wl_shell_surface *shell_surface;
+	struct wl_buffer *buffer;
+	int has_argb;
+	int width, height;
+	void *data;
+};
+
+static void
+create_shm_buffer(struct touch *touch)
+{
+	struct wl_shm_pool *pool;
+	int fd, size, stride;
+
+	stride = touch->width * 4;
+	size = stride * touch->height;
+
+	fd = os_create_anonymous_file(size);
+	if (fd < 0) {
+		LOGE("creating a buffer file for %d B failed: %s\n", size,
+		     strerror(errno));
+		exit(1);
+	}
+
+	touch->data =
+		mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (touch->data == MAP_FAILED) {
+		LOGE("mmap failed: %s\n", strerror(errno));
+		close(fd);
+		exit(1);
+	}
+
+	pool = wl_shm_create_pool(touch->shm, fd, size);
+	touch->buffer =
+		wl_shm_pool_create_buffer(pool, 0,
+					  touch->width, touch->height, stride,
+					  WL_SHM_FORMAT_ARGB8888);
+	wl_shm_pool_destroy(pool);
+
+	close(fd);
+}
+
+static void
+shm_format(void *data, struct wl_shm *wl_shm, uint32_t format)
+{
+	struct touch *touch = data;
+
+	if (format == WL_SHM_FORMAT_ARGB8888)
+		touch->has_argb = 1;
+}
+
+struct wl_shm_listener shm_listener = {
+	shm_format
+};
+
+
+static void
+touch_paint(struct touch *touch, int32_t x, int32_t y, int32_t id)
+{
+	uint32_t *p, c;
+	static const uint32_t colors[] = {
+		0xffff0000,
+		0xffffff00,
+		0xff0000ff,
+		0xffff00ff,
+		0xff00ff00,
+		0xff00ffff,
+	};
+
+	if (id < (int32_t) ARRAY_LENGTH(colors))
+		c = colors[id];
+	else
+		c = 0xffffffff;
+
+	if (x < 2 || x >= touch->width - 2 ||
+	    y < 2 || y >= touch->height - 2)
+		return;
+
+	p = (uint32_t *) touch->data + (x - 2) + (y - 2) * touch->width;
+	p[2] = c;
+	p += touch->width;
+	p[1] = c;
+	p[2] = c;
+	p[3] = c;
+	p += touch->width;
+	p[0] = c;
+	p[1] = c;
+	p[2] = c;
+	p[3] = c;
+	p[4] = c;
+	p += touch->width;
+	p[1] = c;
+	p[2] = c;
+	p[3] = c;
+	p += touch->width;
+	p[2] = c;
+
+	wl_surface_attach(touch->surface, touch->buffer, 0, 0);
+	wl_surface_damage(touch->surface, x - 2, y - 2, 5, 5);
+	/* todo: We could queue up more damage before committing, if there
+	 * are more input events to handle.
+	 */
+	wl_surface_commit(touch->surface);
+}
+
+static void
+touch_handle_down(void *data, struct wl_touch *wl_touch,
+		  uint32_t serial, uint32_t time, struct wl_surface *surface,
+		  int32_t id, wl_fixed_t x_w, wl_fixed_t y_w)
+{
+	struct touch *touch = data;
+	float x = wl_fixed_to_double(x_w);
+	float y = wl_fixed_to_double(y_w);
+
+	touch_paint(touch, x, y, id);
+}
+
+static void
+touch_handle_up(void *data, struct wl_touch *wl_touch,
+		uint32_t serial, uint32_t time, int32_t id)
+{
+}
+
+static void
+touch_handle_motion(void *data, struct wl_touch *wl_touch,
+		    uint32_t time, int32_t id, wl_fixed_t x_w, wl_fixed_t y_w)
+{
+	struct touch *touch = data;
+	float x = wl_fixed_to_double(x_w);
+	float y = wl_fixed_to_double(y_w);
+
+	touch_paint(touch, x, y, id);
+}
+
+static void
+touch_handle_frame(void *data, struct wl_touch *wl_touch)
+{
+}
+
+static void
+touch_handle_cancel(void *data, struct wl_touch *wl_touch)
+{
+}
+
+static const struct wl_touch_listener touch_listener = {
+	touch_handle_down,
+	touch_handle_up,
+	touch_handle_motion,
+	touch_handle_frame,
+	touch_handle_cancel,
+};
+
+static void
+seat_handle_capabilities(void *data, struct wl_seat *seat,
+			 enum wl_seat_capability caps)
+{
+	struct touch *touch = data;
+
+	if ((caps & WL_SEAT_CAPABILITY_TOUCH) && !touch->wl_touch) {
+		touch->wl_touch = wl_seat_get_touch(seat);
+		wl_touch_set_user_data(touch->wl_touch, touch);
+		wl_touch_add_listener(touch->wl_touch, &touch_listener, touch);
+	} else if (!(caps & WL_SEAT_CAPABILITY_TOUCH) && touch->wl_touch) {
+		wl_touch_release(touch->wl_touch);
+		touch->wl_touch = NULL;
+	}
+}
+
+static const struct wl_seat_listener seat_listener = {
+	seat_handle_capabilities,
+};
+
+static void
+handle_ping(void *data, struct wl_shell_surface *shell_surface,
+	    uint32_t serial)
+{
+	wl_shell_surface_pong(shell_surface, serial);
+}
+
+static void
+handle_configure(void *data, struct wl_shell_surface *shell_surface,
+		 uint32_t edges, int32_t width, int32_t height)
+{
+}
+
+static void
+handle_popup_done(void *data, struct wl_shell_surface *shell_surface)
+{
+}
+
+static const struct wl_shell_surface_listener shell_surface_listener = {
+	handle_ping,
+	handle_configure,
+	handle_popup_done
+};
+
+static void
+handle_global(void *data, struct wl_registry *registry,
+	      uint32_t name, const char *interface, uint32_t version)
+{
+	struct touch *touch = data;
+
+	if (strcmp(interface, "wl_compositor") == 0) {
+		touch->compositor =
+			wl_registry_bind(registry, name,
+					 &wl_compositor_interface, 1);
+	} else if (strcmp(interface, "wl_shell") == 0) {
+		touch->shell =
+			wl_registry_bind(registry, name,
+					 &wl_shell_interface, 1);
+	} else if (strcmp(interface, "wl_fullscreen_shell") == 0) {
+		touch->fullscreen_shell =
+			wl_registry_bind(registry, name,
+					 &wl_fullscreen_shell_interface, 1);
+	} else if (strcmp(interface, "wl_shm") == 0) {
+		touch->shm = wl_registry_bind(registry, name,
+					      &wl_shm_interface, 1);
+		wl_shm_add_listener(touch->shm, &shm_listener, touch);
+	} else if (strcmp(interface, "wl_seat") == 0) {
+		touch->seat = wl_registry_bind(registry, name,
+					       &wl_seat_interface, 1);
+		wl_seat_add_listener(touch->seat, &seat_listener, touch);
+	}
+}
+
+static void
+handle_global_remove(void *data, struct wl_registry *registry, uint32_t name)
+{
+}
+
+static const struct wl_registry_listener registry_listener = {
+	handle_global,
+	handle_global_remove
+};
+
+static struct touch *
+touch_create(int width, int height)
+{
+	struct touch *touch;
+
+	touch = malloc(sizeof *touch);
+	if (touch == NULL) {
+		LOGE("out of memory\n");
+		exit(1);
+	}
+	touch->display = wl_display_connect(NULL);
+	assert(touch->display);
+
+	touch->has_argb = 0;
+	touch->registry = wl_display_get_registry(touch->display);
+	wl_registry_add_listener(touch->registry, &registry_listener, touch);
+	wl_display_dispatch(touch->display);
+	wl_display_roundtrip(touch->display);
+
+	if (!touch->has_argb) {
+		LOGE("WL_SHM_FORMAT_ARGB32 not available\n");
+		exit(1);
+	}
+
+	touch->width = width;
+	touch->height = height;
+	touch->surface = wl_compositor_create_surface(touch->compositor);
+
+	create_shm_buffer(touch);
+
+	if (touch->fullscreen_shell) {
+		wl_fullscreen_shell_present_surface(touch->fullscreen_shell,
+						    touch->surface,
+						    WL_FULLSCREEN_SHELL_PRESENT_METHOD_DEFAULT,
+						    0, NULL);
+	} else if (touch->shell) {
+		touch->shell_surface =
+			wl_shell_get_shell_surface(touch->shell,
+						   touch->surface);
+		wl_shell_surface_add_listener(touch->shell_surface,
+					      &shell_surface_listener, touch);
+		wl_shell_surface_set_toplevel(touch->shell_surface);
+	}
+
+	wl_surface_set_user_data(touch->surface, touch);
+	wl_shell_surface_set_title(touch->shell_surface, "simple-touch");
+
+	memset(touch->data, 64, width * height * 4);
+	wl_surface_attach(touch->surface, touch->buffer, 0, 0);
+	wl_surface_damage(touch->surface, 0, 0, width, height);
+	wl_surface_commit(touch->surface);
+
+	return touch;
+}
+
+int
+main(int argc, char **argv)
+{
+	struct touch *touch;
+	int ret = 0;
+
+	touch = touch_create(600, 500);
+
+	while (ret != -1)
+		ret = wl_display_dispatch(touch->display);
+
+	return 0;
+}
